f45503cf37606b343a7891810933d97c8f405f22
Buff4eyes <claudemokbel@gmail.com>
2022-01-20 19:10:35 -0800

    navbar renders, still needs styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './round-clm.png';
 import './App.css';
+import { Navigation } from './components'
 
 function App() {
   return (
+    <>
+    <Navigation />
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Claude Mokbel
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View Portfolio
-        </a>
       </header>
     </div>
+  </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {  Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" light expand="md">
+          <NavbarBrand href="/">Claude Mokbel</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#portfolio">Portfolio</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#resume">Resume</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/cmokbel1">GitHub</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
