a2cd1b10910b7004a3327c2d7358b40f13b06e09
Buff4eyes <claudemokbel@gmail.com>
2022-01-23 14:09:30 -0800

    projects array created

--- src/projects.js
+++ src/projects.js
@@ -1,58 +1,72 @@
 const projects = [
   {
-    title: '',
-    description:'',
-    gitHub: '',
-    liveSite: '',
+    title: '4uWorkout',
+    description:'This application allows users to generate random workouts based on specific body parts while simultaneously generating a playlist of music to go along with the workout.',
+    gitHub: 'https://github.com/cmokbel1/project_1',
+    liveSite: 'https://jazminejose.github.io/project_1/',
+    img: './images/4uWorkout.png'
+  },
+  {
+    title: 'Read Me Generator',
+    description: 'This project was built to allow myself and other users the opportunity to create a README.md file without the hassle of manually inputting the information needed.',
+    gitHub: 'https://github.com/cmokbel1/ReadMeGenerator',
+    liveSite: 'https://www.youtube.com/watch?v=yLcLP55G0d0',
     img: ''
   },
   {
-    title: '',
-    description: '',
-    gitHub: '',
-    liveSite: '',
+    title: 'Social Media API',
+    description: 'This project was built as a back-end development for a newly made social media company. One can create users, retrieve all or one of them, update a user and delete users. Further one can add friends and remove them. Users have the capability to create posts and leave comments via thoughts and reactions. We can delete thoughts and reactions as well. This project using a mongoDB that allows for seamless storage of unrelated data and makes it easy to retrieve as well.',
+    gitHub: 'https://github.com/cmokbel1/Social-Media-API',
+    liveSite: 'https://www.youtube.com/watch?v=D4DBZyZB4ro',
     img: ''
   },
   {
-    title: '',
-    description: '',
-    gitHub: '',
-    liveSite: '',
-    img: ''
+    title: 'Day Planner',
+    description: 'Users can open the webpage and plan out there day with a seamless process. One can save information by clicking save, and simply click the text area to edit and save again. When the hour comes to pass the slot will turn gray. when the hour is current it is red, and when it is a future time the slot is green.',
+    gitHub: 'https://github.com/cmokbel1/Day-Planner',
+    liveSite: 'https://cmokbel1.github.io/Day-Planner/',
+    img: './images/dayplanner.png'
   },
   {
-    title: '',
+    title: 'Chore Door Game',
     description: '',
-    gitHub: '',
-    liveSite: '',
-    img: ''
+    gitHub: 'https://github.com/cmokbel1/Chore-Door-Game-Clone',
+    liveSite: 'https://cmokbel1.github.io/Chore-Door-Game-Clone/',
+    img: './images/chore-door.png'
   },
   {
-    title: '',
+    title: 'Weather Forecast',
     description: '',
-    gitHub: '',
-    liveSite: '',
-    img: ''
+    gitHub: 'https://github.com/cmokbel1/WeatherAPI',
+    liveSite: 'https://cmokbel1.github.io/WeatherAPI/',
+    img: './images/weatherAPI.png'
   },
   {
-    title: '',
-    description: '',
-    gitHub: '',
-    liveSite: '',
-    img: ''
+    title: 'Tech Blog',
+    description: 'This project is a combination of methods learned in Javascript. Models and Routes are used to populate a seemingly empty webpage with blog posts created by users that are logged in via passport and authenticated with a json webtoken. User information is stored in a JAWSDB database using mySQL tables. Further, Users can comment on other posts made by other users given they are registered on the website.',
+    gitHub: 'https://github.com/cmokbel1/Tech-Blog',
+    liveSite: 'https://blog-about-it-techblog.herokuapp.com',
+    img: './images/tbl.png'
   },
   {
-    title: '',
-    description: '',
-    gitHub: '',
-    liveSite: '',
-    img: ''
+    title: 'Employee Tracker',
+    description: 'This project is a collaboration of multiple parts. A mySQL back-end server is connected to multiple files in Javascript that allow the user to run the application wholely in the terminal. Users have the option to track employee data. One can view departments, roles, and employees. Further a user has the opportunity to add employees, roles, and departments. Along with these features the user can also delete employees, roles, or departments. Additional features include updating employee roles, and updating the manager of an employee.',
+    gitHub: 'https://github.com/cmokbel1/Employee-Tracker',
+    liveSite: 'https://www.youtube.com/watch?v=ZTYxlquJVmk',
+    img: './images/employee-tracker.png'
   },
   {
-    title: '',
-    description: '',
-    gitHub: '',
-    liveSite: '',
-    img: ''
+    title: 'Chat Wallet',
+    description: 'Chat Wallet is an web based app that allows users to chat with other individuals about each others hobbies and interests. Users can register for an account, sign in, sign out and enter varying chatrooms that are developed using socket.io. this application uses passport and jsonwebtoken to authenticate users and verify user information for access to profile information and the chat rooms.',
+    gitHub: 'https://github.com/cmokbel1/chatWallet',
+    liveSite: 'chat-wallet.herokuapp.com/',
+    img: './images/CW.png'
+  },
+  {
+    title: 'Password Generator',
+    description: 'an application that randomizes passwords for users based on user specifications. Passwords can be between 8 and 128 characters long, contain uppercase or lowercase letter, special characters, or numbers.',
+    gitHub: 'https://github.com/cmokbel1/Password-Generator',
+    liveSite: 'https://cmokbel1.github.io/Password-Generator/ ',
+    img: './images/pwgen.png'
   }
 ]
