5332ad6ba80c7680c7d4ac1ea71d2e4090a35663
Buff4eyes <claudemokbel@gmail.com>
2022-01-23 15:35:32 -0800

    projects render correctly.

--- src/components/Project/ProjCard.js
+++ src/components/Project/ProjCard.js
@@ -1,19 +1,26 @@
-import { Card, Button } from 'reactstrap'
+import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, Button
+} from 'reactstrap';
 
 const ProjCard = (props) => {
   const { project } = props;
   return (
+    <>
     <Card border="dark" background-color="light">
-      <Card.Img variant="top" src={project.img} />
-      <Card.Body>
-        <Card.Title>{project.title}</Card.Title>
-        <Card.Text>
+      <CardImg variant="top" src={project.img} />
+      <CardBody>
+        <CardTitle>{project.title}</CardTitle>
+        <CardText>
          {project.description}
-        </Card.Text>
-        <Button variant="primary">{project.gitHub}</Button>
-        <Button variant="primary">{project.liveSite}</Button>
-      </Card.Body>
+        </CardText>
+          <Button variant="primary" color="info" href={project.gitHub}>GitHub</Button>
+          <br />
+          <Button variant="primary" color="danger" href={project.liveSite}>Live View</Button>
+      </CardBody>
     </Card>
+    </>
   )
 }
 
