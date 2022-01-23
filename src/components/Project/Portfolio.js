33d8d2300172f5d65ee76c60deb3a231de0d7b15
Buff4eyes <claudemokbel@gmail.com>
2022-01-23 12:56:43 -0800

    Portfolio component

---
+++ src/components/Project/Portfolio.js
@@ -0,0 +1,12 @@
+import ProjCard from './ProjCard'
+
+const Portfolio = (props) => {
+  const {projects} = props;
+  return (
+    <div className="row">
+      {projects.map((project,idx) => <ProjCard key={idx} project={project}/>)};
+    </div>
+  )
+}
+
+export default Portfolio;
