6f503e6b524d17ad270d7fbd0a2fc5e7604d1db7
Buff4eyes <claudemokbel@gmail.com>
2022-01-24 10:28:13 -0800

    contact form created

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,33 @@
 import { Component } from 'react'
+import {
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from 'reactstrap';
 
 class Contact extends Component {
-
+render() {
+ return (
+   <div className="container">
+     <Form>
+       <FormGroup>
+         <Label for="name">Name</Label>
+         <Input id="name" name="name" type="text" placeholder="First Name Last Name"></Input>
+       </FormGroup>
+       <FormGroup>
+         <Label for="name">e-mail</Label>
+         <Input id="email" name="email" type="text" placeholder="firstname@email.com"></Input>
+       </FormGroup>
+       <FormGroup>
+         <Label for="name">Message</Label>
+         <Input id="message" name="message" type="textarea" placeholder="Hey Claude really loved what I saw in your portfolio and your resume! We would love to hire you on for our upcoming project."></Input>
+       </FormGroup>
+     </Form>
+   </div>
+ )
+}
 }
 
 export default Contact
