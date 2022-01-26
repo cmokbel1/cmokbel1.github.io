30ace0c9b995b88c0466c7f8cc74f5abbce07f76
Buff4eyes <claudemokbel@gmail.com>
2022-01-26 15:15:51 -0800

    rearranged projects

--- src/projects.js
+++ src/projects.js
@@ -11,8 +11,29 @@ import image10 from './images/pwgen.png';
 
 const projects = [
   {
+    title: 'Chat Wallet',
+    description: 'Chat Wallet is an web based app that allows users to chat with other individuals about each others hobbies and interests. Users can register for an account, sign in, sign out and enter varying chatrooms that are developed using socket.io. this application uses passport and jsonwebtoken to authenticate users and verify user information for access to profile information and the chat rooms.',
+    gitHub: 'https://github.com/cmokbel1/chatWallet',
+    liveSite: 'chat-wallet.herokuapp.com/',
+    img: image9
+  },
+  {
+    title: 'Tech Blog',
+    description: 'This project is a combination of methods learned in Javascript. Models and Routes are used to populate a seemingly empty webpage with blog posts created by users that are logged in via passport and authenticated with a json webtoken. User information is stored in a JAWSDB database using mySQL tables. Further, Users can comment on other posts made by other users given they are registered on the website.',
+    gitHub: 'https://github.com/cmokbel1/Tech-Blog',
+    liveSite: 'https://blog-about-it-techblog.herokuapp.com',
+    img: image7
+  },
+    {
+    title: 'Social Media API',
+    description: 'This project was built as a back-end development for a newly made social media company. One can create users, retrieve all or one of them, update a user and delete users. Further one can add friends and remove them. Users have the capability to create posts and leave comments via thoughts and reactions. We can delete thoughts and reactions as well. This project using a mongoDB that allows for seamless storage of unrelated data and makes it easy to retrieve as well.',
+    gitHub: 'https://github.com/cmokbel1/Social-Media-API',
+    liveSite: 'https://www.youtube.com/watch?v=D4DBZyZB4ro',
+    img: image3
+  },
+  {
     title: '4uWorkout',
-    description:'This application allows users to generate random workouts based on specific body parts while simultaneously generating a playlist of music to go along with the workout.',
+    description: 'This application allows users to generate random workouts based on specific body parts while simultaneously generating a playlist of music to go along with the workout.',
     gitHub: 'https://github.com/cmokbel1/project_1',
     liveSite: 'https://jazminejose.github.io/project_1/',
     img: image1
@@ -23,13 +44,6 @@ const projects = [
     gitHub: 'https://github.com/cmokbel1/ReadMeGenerator',
     liveSite: 'https://www.youtube.com/watch?v=yLcLP55G0d0',
     img: image2
-  },
-  {
-    title: 'Social Media API',
-    description: 'This project was built as a back-end development for a newly made social media company. One can create users, retrieve all or one of them, update a user and delete users. Further one can add friends and remove them. Users have the capability to create posts and leave comments via thoughts and reactions. We can delete thoughts and reactions as well. This project using a mongoDB that allows for seamless storage of unrelated data and makes it easy to retrieve as well.',
-    gitHub: 'https://github.com/cmokbel1/Social-Media-API',
-    liveSite: 'https://www.youtube.com/watch?v=D4DBZyZB4ro',
-    img: image3
   },
   {
     title: 'Day Planner',
@@ -53,25 +67,11 @@ const projects = [
     img: image6
   },
   {
-    title: 'Tech Blog',
-    description: 'This project is a combination of methods learned in Javascript. Models and Routes are used to populate a seemingly empty webpage with blog posts created by users that are logged in via passport and authenticated with a json webtoken. User information is stored in a JAWSDB database using mySQL tables. Further, Users can comment on other posts made by other users given they are registered on the website.',
-    gitHub: 'https://github.com/cmokbel1/Tech-Blog',
-    liveSite: 'https://blog-about-it-techblog.herokuapp.com',
-    img: image7
-  },
-  {
     title: 'Employee Tracker',
     description: 'This project is a collaboration of multiple parts. A mySQL back-end server is connected to multiple files in Javascript that allow the user to run the application wholely in the terminal. Users have the option to track employee data. One can view departments, roles, and employees. Further a user has the opportunity to add employees, roles, and departments. Along with these features the user can also delete employees, roles, or departments. Additional features include updating employee roles, and updating the manager of an employee.',
     gitHub: 'https://github.com/cmokbel1/Employee-Tracker',
     liveSite: 'https://www.youtube.com/watch?v=ZTYxlquJVmk',
     img: image8
-  },
-  {
-    title: 'Chat Wallet',
-    description: 'Chat Wallet is an web based app that allows users to chat with other individuals about each others hobbies and interests. Users can register for an account, sign in, sign out and enter varying chatrooms that are developed using socket.io. this application uses passport and jsonwebtoken to authenticate users and verify user information for access to profile information and the chat rooms.',
-    gitHub: 'https://github.com/cmokbel1/chatWallet',
-    liveSite: 'chat-wallet.herokuapp.com/',
-    img: image9
   },
   {
     title: 'Password Generator',
