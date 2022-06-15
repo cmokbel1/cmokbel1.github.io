b3731bbd5f9e19598957775d065afa56daa7c0e6
Claude Mokbel <claudemokbel@gmail.com>
2022-06-15 09:19:26 -0700

    updated paragraph

--- src/components/Project/Portfolio.js
+++ src/components/Project/Portfolio.js
@@ -2,17 +2,21 @@ import ProjCard from './ProjCard'
 import './portfolio.css'
 
 const Portfolio = (props) => {
-  const {projects} = props;
+  const { projects } = props;
   return (
     <>
-    <div className="title container"><h1>Claude Mokbel</h1>
-    <p>A recent graduate of the UCI Continued Education Full Stack Web Developer bootcamp. I have 6 months of experience building web applications in HTML, CSS, and Javascript. I am currently focused on honing my skills in React.js, learning patterns as well as algorithms, and database functionality in web applications.</p>
-    </div>
-    <h3 className="title"><strong><u>Projects</u></strong></h3>
-    <br></br>
-    <div className="row justify-content-center text-center mx-auto">
-      {projects.map((project, idx) => <ProjCard key={idx} project={project}/>)}
-    </div>
+      <div className="title container"><h1>Claude Mokbel</h1>
+        <p>A recent graduate of the UCI Continued Education Full Stack Web Developer bootcamp.
+          I have 7 months of experience building web applications in HTML, CSS, and Javascript with mobile responsivity.
+          I am currently focused on honing my skills in React.js, learning patterns as well as algorithms,
+          and database functionality in web applications.
+        </p>
+      </div>
+      <h3 className="title"><strong><u>Projects</u></strong></h3>
+      <br></br>
+      <div className="row justify-content-center text-center mx-auto">
+        {projects.map((project, idx) => <ProjCard key={idx} project={project} />)}
+      </div>
     </>
   )
 }
