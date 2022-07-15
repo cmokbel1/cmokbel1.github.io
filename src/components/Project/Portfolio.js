6a48d3c2ad9a40cfed0ffc3916304c6ec0098fe6
Claude Mokbel <claudemokbel@gmail.com>
2022-07-15 13:52:26 -0700

    updated site header

--- src/components/Project/Portfolio.js
+++ src/components/Project/Portfolio.js
@@ -7,9 +7,8 @@ const Portfolio = (props) => {
     <>
       <div className="title container"><h1>Claude Mokbel</h1>
         <p>A recent graduate of the UCI Continued Education Full Stack Web Developer bootcamp.
-          I have 7 months of experience building web applications in HTML, CSS, and Javascript with mobile responsivity.
-          I am currently focused on honing my skills in React.js, learning patterns as well as algorithms,
-          and database functionality in web applications.
+          I have 8 months of experience building web applications in HTML, CSS, and Javascript with mobile responsivity.
+          I am currently focused on learning data structures as well as algorithms, and improving my javascript skills.
         </p>
       </div>
       <h3 className="title"><strong><u>Projects</u></strong></h3>
